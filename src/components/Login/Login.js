6e1c3da446f6779c8e2310179c54d51f12709b71
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-04-29 19:59:01 +0430

    Using the useEffect CLEANUP

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,15 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6 
-    );
+     const identifier =  setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      clearTimeout(identifier)
+    }
   }, [enteredEmail , enteredPassword])
 
 
@@ -22,7 +28,7 @@ const Login = (props) => {
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
 
-    
+    //
   };
 
   const passwordChangeHandler = (event) => {
